e930ff7e9bd976a13daea78c3d93f3eb4b238c6f
SOYEONG OH <axionoso@gmail.com>
2019-03-28 21:05:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,7 +30,7 @@ def nyc_pigeon_organizer(data)
           new_hash[name][:color] = []
         end
         if attribute_category == :color
-          new_hash[name][attribute_category] << attribute]
+          new_hash[name][attribute_category] << attribute
         else
           new_hash[name][attribute_category] = attribute.to_s
         end
