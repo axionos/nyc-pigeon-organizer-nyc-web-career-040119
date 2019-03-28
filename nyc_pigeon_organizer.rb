c9cbb3cb829ae78439ac9d706d62130591727eb5
SOYEONG OH <axionoso@gmail.com>
2019-03-28 20:22:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,9 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |att, array|
       array.each do |name|
+        if new_hash.has_key?("name")
 
+        end
     end
   end
 end
